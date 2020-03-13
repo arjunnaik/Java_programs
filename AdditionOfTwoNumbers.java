1b576f7828e6be1cd4ca88ce1b3d7bda8ec7ee0e
arjunnaik <55954515+arjunnaik@users.noreply.github.com>
2020-03-13 20:05:27 +0530

    Add files via upload

---
+++ AdditionOfTwoNumbers.java
@@ -0,0 +1,15 @@
+package work;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+	   int num1,num2,ans;
+	   num1 = 4 ;
+	   num2 = 10 ;
+	   ans = num1 + num2 ;
+	   System.out.println(ans);
+	   
+      
+	}
+
+}
