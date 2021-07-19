0d5c094c40d1fe8a8a8c9502bf5b93a6f2d8c905
Arjun Naik <55954515+arjunnaik@users.noreply.github.com>
2021-07-19 22:15:20 +0530

    Create Time Conversion.java

---
+++ Time Conversion.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    String tArr[] = s.split(":");
+        String AmPm = tArr[2].substring(2,4);
+        int hh,mm,ss;
+        hh = Integer.parseInt(tArr[0]);
+        mm = Integer.parseInt(tArr[1]);
+        ss = Integer.parseInt(tArr[2].substring(0,2));
+        
+        String checkPM = "PM",checkAM ="AM";
+        int h = hh;
+        if(AmPm.equals(checkAM) && hh==12)
+            h=0;
+        else if(AmPm.equals(checkPM)&& hh<12)
+            h+=12;
+        
+        String result=String.format("%02d:%02d:%02d",h,mm,ss);
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
